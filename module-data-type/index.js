27594232f5543b7e7ca5edb6c990bdf76dcb3ec9
Sabun Tombak <yaziddr72@gmail.com>
2025-06-01 15:29:26 +0700

    menambahkan module-type-data

---
+++ module-data-type/index.js
@@ -0,0 +1,35 @@
+// string , digunakan untuk memanipulasi text
+// number, digunakan untuk menyimpan bilangan numerik termasuk ganjil genap
+// Boolean, hanya memiliki nilai: true or false.
+
+// >>>>> practice
+// number
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// string
+// let namaDepan = "Nomad";
+// let namaTengah = "Kacau";
+// let namaBelakang = "Dev";
+// console.log(typeof namaDepan);
+// console.log(`nama anda adalah ${namaDepan} ${namaTengah} ${namaBelakang}`);
+
+// boolean
+// let online = true;
+// let iniDijual = false;
+// let masihSekolah = true;
+
+// console.log(typeof online);
+// console.log(`Nomad telah online: ${online}`);
+// console.log(`apakah ini dijual ${iniDijual}`);
+// console.log(`apakah kamu masih sekolah ${masihSekolah}`);
+
+// latihan dengan mengambil elemnt id di html
+// let nama = "Nomad";
+// let age = 25;
+// let siswa = false;
+
+// document.getElementById("p1").textContent = `Nama anda adalah ${nama}`;
+// document.getElementById("p2").textContent = `Umur anda adalah ${age} Tahun`;
+// document.getElementById("p3").textContent = `Anda seorang siswa: ${siswa}`;
